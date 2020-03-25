b09a44f6ed748ea51de4db25bbbafbe37c2226dd
WerHawer <werhawer@gmail.com>
2020-03-25 22:41:14 +0200

    add scroll on btm learn more

--- mogo_page/index.js
+++ mogo_page/index.js
@@ -1,9 +1,19 @@
 const menu = document.querySelector('#js-menu');
 const menuBtn = document.querySelector('.menu-icon');
+const learnMoreBtn = document.querySelector('.banner__button');
+const startInfo = document.querySelector('#startInfo');
 
 const openMobileMenu = () => {
   console.log(menu.classList.contains('nav--open'));
   menu.classList.toggle('nav--open');
 };
 
+const learnMoreFn = () => {
+  startInfo.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+  });
+};
+
 menuBtn.addEventListener('click', openMobileMenu);
+learnMoreBtn.addEventListener('click', learnMoreFn);
